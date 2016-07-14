ed242b7a50e0f0420a642981ab2d7170f9424d33
Chau Quoc Minh <minhb1304568@student.ctu.edu.vn>
2016-07-14 20:43:40 +0700

    Minh: create a TestDAO class

---
+++ AB-SD Java/src/dao/TestDAO.java
@@ -0,0 +1,14 @@
+package dao;
+
+public class TestDAO {
+
+	public TestDAO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
